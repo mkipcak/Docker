b445c8b877811920640bc686708dacda5b95d113
Fede Diaz <nordri@gmail.com>
2018-04-23 17:07:01 +0200

    change datetime by commit id

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -x
 set -e
 
-DATETIME=$(date +"%Y%m%d%H%M%S")
-
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASS
 cd nordri-dev-tools
 docker build -t nordri-dev-tools .
 docker tag nordri-dev-tools nordri/nordri-dev-tools:latest
-docker tag nordri-dev-tools nordri/nordri-dev-tools:$DATETIME
+docker tag nordri-dev-tools nordri/nordri-dev-tools:$TRAVIS_COMMIT
 docker push nordri/nordri-dev-tools:latest
-docker push nordri/nordri-dev-tools:$DATETIME
+docker push nordri/nordri-dev-tools:$TRAVIS_COMMIT
 docker logout
